cb4c7dcafdc9a54de9546519a59375ff55e08525
handiism <mail.handira@gmail.com>
2024-02-09 11:49:59 +0700

    fix: set env to accept from non .env file

--- util/config.go
+++ util/config.go
@@ -1,6 +1,11 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	Environment       string `mapstructure:"ENVIRONMENT"`
@@ -9,17 +14,48 @@ type Config struct {
 	HTTPServerAddress string `mapstructure:"HTTP_SERVER_ADDRESS"`
 }
 
-func LoadConfig(path string) (config Config, err error) {
+func LoadConfig(path string) (Config, error) {
 	viper.AddConfigPath(path)
 	viper.SetConfigFile(".env")
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
+
+	var config Config
+	if _, err := os.Stat(".env"); err != nil {
+		environment, found := os.LookupEnv("ENVIRONMENT")
+		if !found {
+			return config, fmt.Errorf("can't find ENVIRONMENT value")
+		}
+		config.Environment = environment
+
+		postgresURL, found := os.LookupEnv("POSTGRES_URL")
+		if !found {
+			return config, fmt.Errorf("can't find POSTGRES_URL value")
+		}
+		config.PostgresURL = postgresURL
+
+		migrationPath, found := os.LookupEnv("MIGRATION_PATH")
+		if !found {
+			return config, fmt.Errorf("can't find POSTGRES_URL value")
+		}
+		config.MigrationPath = migrationPath
+
+		address, found := os.LookupEnv("HTTP_SERVER_ADDRESS")
+		if !found {
+			return config, fmt.Errorf("can't find HTTP_SERVER_ADDRESS value")
+		}
+		config.HTTPServerAddress = address
+
+		return config, nil
 	}
 
-	err = viper.Unmarshal(&config)
-	return
+	if err := viper.ReadInConfig(); err != nil {
+		return config, err
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
